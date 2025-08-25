2de52aa4b18124705cc079cd5b1d9474553d707b
ANUBIS <ibaioumy663@gmail.com>
2025-08-25 07:55:28 +0300

    adminsite(1)

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin # noqa
+'''
+    django admin customization for the core app.
+'''
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as trans
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ''' Define the admin pages for users.'''
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email','password')}),
+        (trans('Personal Info'), {'fields': ('name',)}),
+        (
+            trans('premissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (trans('important dates'), {'fields': ('last_login',)}
+        )
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,36 @@
+'''test for admin modifications'''
+
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+    '''Test for the Django admin .'''
+
+    def setUp(self):
+        '''Create user and client for testing admin site.'''
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="testpassd123",
+            name="Test User"
+        )
+
+    def test_users_listed(self):
+        '''Test users are listed on admin page.'''
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+
+    def test_user_change_page(self):
+        '''Test that the user edit page works.'''
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
